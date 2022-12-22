6476806bdcc2713d75d6f16a88bd15ccf270fef9
aurore.philippe <asiphilippe@hotmail.com>
2022-12-22 20:07:46 +0100

    Update

--- Chunking/Model/MLT.java
+++ Chunking/Model/MLT.java
@@ -1,35 +1,61 @@
 package Model;
 
 import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.TreeSet;
-import java.sql.*;
 
 public class MLT {
-	
+
 	private TreeSet<String> mots;
 	private int taille;
-	
+
 	public MLT() {
-		mots = new TreeSet<String>();
+		/* initialisation des mots communs contenus dans la base de données 
+		 * et mise à jour de la taille de la collection. */
+		this("accordéon");
+		mots.add("calendrier");
+		mots.add("aspirateur");
+		mots.add("caméléon");
+		mots.add("crocodile");
 		mots.add("dentifrice");
-		
-		taille=mots.size();
+		mots.add("dinosaure");
+		mots.add("distributeur");
+		mots.add("harmonica");
+		mots.add("imprimante");
+		mots.add("épouvantail");
+		mots.add("macaroni");
+		mots.add("mariage");
+		mots.add("médicament");
+		mots.add("ordinateur");
+		mots.add("parachute");
+		mots.add("photographie");
+		mots.add("rhinocéros");
+		mots.add("technologie");
+		mots.add("tentacule");
+		mots.add("trampoline");
+		mots.add("ventilateur");
+		mots.add("apiculteur");
+		mots.add("médecine");
+		mots.add("étudiant");
+		mots.add("ingénieur");
+		mots.add("mécanique");
+		mots.add("avocate");
+		mots.add("boulangerie");
+		mots.add("pharmacien");
+		taille = mots.size();
 	}
-	
+
 	public MLT(String mot) {
 		mots = new TreeSet<String>();
 		mots.add(mot);
-		taille++;
+		taille = mots.size();
 	}
-	
+
 	public MLT(Collection<String> mots) {
 		mots = new TreeSet<String>();
 		this.mots.addAll(mots);
-		taille = taille+mots.size();
+		taille = mots.size();
 	}
-	
+
 	public boolean existe(String s) {
 		// renvoie true si mots contient s, false sinon
 		return mots.contains(s);
@@ -42,8 +68,9 @@ public class MLT {
 	public void setMots(TreeSet<String> mots) {
 		this.mots = mots;
 	}
-	
+
 	public String toString() {
+		// retourne une chaîne de caractère contenant les mots sous forme de liste verticale
 		TreeSet<String> copy = (TreeSet<String>) mots.clone(); 
 		String res = "";
 		if(copy.isEmpty())
@@ -58,11 +85,14 @@ public class MLT {
 		return res;
 	}
 
-	public static void main(String[] args) {
-		/*MLT memoire = new MLT();
+	// test des méthodes 
+	/*public static void main(String[] args) {
+		MLT memoire = new MLT();
+		// test de toString()
 		System.out.println(memoire);
-		System.out.println(memoire.existe("aspirateur"));
-		System.out.println(memoire.existe("dentifrice"));
-		System.out.println(memoire.existe("lit"));*/
-	}
+		// test de existe()
+		System.out.println(memoire.existe("aspirateur")); // doit retourner true
+		System.out.println(memoire.existe("dentifrice")); // doit retourner true
+		System.out.println(memoire.existe("lit")); // doit retourner false
+	}*/
 }
