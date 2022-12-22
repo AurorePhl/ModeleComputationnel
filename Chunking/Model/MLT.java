e7a8635128344de814826b3b388cb143e134370c
kyria1 <99976313+kyria1@users.noreply.github.com>
2022-12-22 16:15:19 +0100

    Update MLT.java

--- Chunking/Model/MLT.java
+++ Chunking/Model/MLT.java
@@ -1,5 +1,14 @@
 package Model;
 
-public class MLT {
+import java.util.Collection;
+import java.util.TreeSet;
 
+public class MLT {
+	Collection<String> mlt;
+	public MLT() {
+		mlt= new TreeSet<String>();
+		mlt.add("aspirateur");
+		mlt.add("cameleon");
+	}
+	
 }
